858216b752f7b518514a031a863889fbea527e37
abbas <learning.abbashaider@gmail.com>
2022-05-29 17:03:42 +0530

    shopcal

---
+++ programs/shopcal.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+
+class shopcla {
+
+    public static void main(String[] args) {
+        float rs, rate, kg, gm;
+        Scanner take = new Scanner(System.in);
+        System.out.println("Type Rs: ");
+        rs = take.nextFloat();
+        System.out.println("Type rate (Per KG): ");
+        rate = take.nextFloat();
+
+        System.out.println(kg + " Kg");
+
+    }
+}
